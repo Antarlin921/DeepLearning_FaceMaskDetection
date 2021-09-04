27b9719e953500b818fe69f486abe99f64e68b8f
Antarlin921 <antarlin921@gmail.com>
2021-09-04 21:55:58 +0530

    cm

--- app.py
+++ app.py
@@ -51,6 +51,6 @@ def contact1():
     return render_template('result.html',predicted_text=f'{output}')
 
 #  run the app  
-if __name__=='__main__':
-    app.run(debug=True,host="0.0.0.0",port=8080)
+# if __name__=='__main__':
+#     app.run(debug=True,host="0.0.0.0",port=8080)
 
